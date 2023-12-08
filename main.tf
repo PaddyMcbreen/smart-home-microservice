be5268d5e54f8f5ae4149d5bddb861b3b19289ee
PaddyMcbreen <patrickmcbreen2004@hotmail.com>
2023-12-08 16:57:01 +0000

    Formatting change made

--- main.tf
+++ main.tf
@@ -3,20 +3,20 @@ module "vpc" {
   source = "./modules/vpc"
 
   // VPC Settings:
-  name = var.name
-  azs_config      = var.azs_config 
+  name       = var.name
+  azs_config = var.azs_config
 
   // Public Subnet Settings:
-  publicSubnets_name = var.publicSubnets_name 
+  publicSubnets_name  = var.publicSubnets_name
   public_subnet_count = var.public_subnet_count
 
   // Private Subnet Settings:
-  privateSubnets_name = var.privateSubnets_name
+  privateSubnets_name  = var.privateSubnets_name
   private_subnet_count = var.private_subnet_count
 
   // Internet Gateway Settings:
   enable_internet_gateway = var.enable_internet_gateway
-  ig_name = var.ig_name
+  ig_name                 = var.ig_name
 
   // Nat Internet Gateway Settings:
   nat_name = var.nat_name
@@ -34,45 +34,45 @@ module "security_group" {
   source = "./modules/security_groups"
 
   // Security Group Settings:
-  vpc_id = module.vpc.vpc_id
+  vpc_id  = module.vpc.vpc_id
   sg_name = var.sg_name
 
   // HTTP ipv6 Ingress Settings:
-  cidr_ipv6_ingress = var.cidr_ipv6_ingress
-  from_port_ipv6 = var.from_port_ipv6
+  cidr_ipv6_ingress        = var.cidr_ipv6_ingress
+  from_port_ipv6           = var.from_port_ipv6
   ip_protocol_ingress_ipv6 = var.ip_protocol_ingress_ipv6
-  to_port_ipv6 = var.to_port_ipv6
+  to_port_ipv6             = var.to_port_ipv6
 
   // HTTP ipv4 Ingress Settings:
-  cidr_ipv4_ingress = var.cidr_ipv4_ingress
-  from_port_ipv4 = var.from_port_ipv4
+  cidr_ipv4_ingress        = var.cidr_ipv4_ingress
+  from_port_ipv4           = var.from_port_ipv4
   ip_protocol_ingress_ipv4 = var.ip_protocol_ingress_ipv4
-  to_port_ipv4 = var.to_port_ipv4
+  to_port_ipv4             = var.to_port_ipv4
 
   // Local Route ipv6 Settings:
-  cidr_ipv6_local = var.cidr_ipv6_local
-  from_localPort_ipv6 = var.from_localPort_ipv6
+  cidr_ipv6_local        = var.cidr_ipv6_local
+  from_localPort_ipv6    = var.from_localPort_ipv6
   ip_protocol_local_ipv6 = var.ip_protocol_local_ipv6
-  to_localPort_ipv6 = var.to_localPort_ipv6
+  to_localPort_ipv6      = var.to_localPort_ipv6
 
   // Local Route ipv4 Settings:
-  cidr_ipv4_local = var.cidr_ipv4_local
-  from_localPort_ipv4 = var.from_localPort_ipv4
+  cidr_ipv4_local        = var.cidr_ipv4_local
+  from_localPort_ipv4    = var.from_localPort_ipv4
   ip_protocol_local_ipv4 = var.ip_protocol_local_ipv4
-  to_localPort_ipv4 = var.to_localPort_ipv4
+  to_localPort_ipv4      = var.to_localPort_ipv4
 
   // ipv6 Egress Settings:
-  cidr_ipv6_egress = var. cidr_ipv6_egress
+  cidr_ipv6_egress        = var.cidr_ipv6_egress
   ip_protocol_egress_ipv6 = var.ip_protocol_egress_ipv6
 
   // ipv4 Egress Settings:
-  cidr_ipv4_egress = var.cidr_ipv4_egress
+  cidr_ipv4_egress        = var.cidr_ipv4_egress
   ip_protocol_egress_ipv4 = var.ip_protocol_egress_ipv4
 
   // SSH Ingress Settings:
-  from_port_ssh = var.from_port_ssh
+  from_port_ssh   = var.from_port_ssh
   ip_protocol_ssh = var.ip_protocol_ssh
-  to_port_ssh = var.to_port_ssh
+  to_port_ssh     = var.to_port_ssh
 }
 
 
@@ -84,22 +84,22 @@ module "microservices" {
   key_name = var.key_name
 
   // AMI Settings:
-  num_ami = var.num_ami
-  ami_names = var.ami_names
+  num_ami        = var.num_ami
+  ami_names      = var.ami_names
   ami_filterName = var.ami_filterName
 
   // Ec2 Settings:
-  num_ec2 = var.num_ec2
-  ec2_names = var.ec2_names
+  num_ec2            = var.num_ec2
+  ec2_names          = var.ec2_names
   instance_type      = var.instance_type
   security_group_ids = module.security_group.security_group_id
   public_subnets_ids = module.vpc.public_subnets_ids
 
   // DynamoDB Settings:
-  dynamo_num = var.dynamo_num
+  dynamo_num  = var.dynamo_num
   dynamo_name = var.dynamo_name
-  read_cap = var.read_cap 
-  write_cap = var.write_cap
+  read_cap    = var.read_cap
+  write_cap   = var.write_cap
 }
 
 
@@ -113,20 +113,20 @@ module "load_balancer" {
   security_group_ids = [module.security_group.security_group_id]
 
   // Target Group Settings:
-  vpc_id = module.vpc.vpc_id
-  tg_name = var.tg_name
-  tg_port = var.tg_port
-  tg_protocol = var.tg_protocol
+  vpc_id           = module.vpc.vpc_id
+  tg_name          = var.tg_name
+  tg_port          = var.tg_port
+  tg_protocol      = var.tg_protocol
   num_targetGroups = var.num_targetGroups
 
   // Target Group Attachment Settings:
-  num_ec2 = [module.microservices.num_ec2]
+  num_ec2      = [module.microservices.num_ec2]
   instance_ids = module.microservices.instance_ids
 
   // Lb Listner Settings:
   lb_listner_name = var.lb_listner_name
-  lb_port = var.lb_port
-  lb_protocol = var.lb_protocol
+  lb_port         = var.lb_port
+  lb_protocol     = var.lb_protocol
 }
 
 
@@ -135,19 +135,19 @@ module "auto_scaling" {
   source = "./modules/auto_scaling"
 
   // Auto Scaling Settings:
-  name_asg = var.name_asg
-  min_size                  = var.min_size  
-  max_size                  = var.max_size  
-  desired_size              = var.desired_size   
+  name_asg                  = var.name_asg
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  desired_size              = var.desired_size
   health_check_grace_period = var.health_check_grace_period
   public_subnets_ids        = module.vpc.public_subnets_ids
 
   // AMI Stuff:
-  ami_names = var.ami_names
+  ami_names     = var.ami_names
   ami_names_rep = var.ami_names_rep
 
   num_launchTemplates = var.num_launchTemplates
-  num_asg             = var.num_asg 
+  num_asg             = var.num_asg
 
   // Launch Template Settings:
   instance_type = var.instance_type
@@ -157,7 +157,7 @@ module "auto_scaling" {
 
   // lt_name array and image_id array must match the num_launchTemplates amount
   lt_name  = var.lt_name
-  image_id = var. image_id
+  image_id = var.image_id
 
   key_name = module.microservices.key_name
 }
--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "public_subnet_count" {
 
 // azs config:
 variable "azs_config" {
-  type    = string
+  type = string
 }
 
 // Private Subnets: 
@@ -23,241 +23,241 @@ variable "private_subnet_count" {
 
 // internet gateway option
 variable "enable_internet_gateway" {
-    type = bool
+  type = bool
 }
 
 variable "publicSubnets_name" {
-  type = string 
+  type        = string
   description = "The names of your public subnets"
 }
 
 variable "privateSubnets_name" {
-  type = string 
+  type        = string
   description = "The names of your private subnets"
 }
 
 variable "ig_name" {
-  type = string 
+  type        = string
   description = "The name of your internet gateway"
 }
 
 variable "public_rt_name" {
-  type = string 
+  type        = string
   description = "The name of your public route table"
 }
 
 
 variable "private_rt_names" {
-  type = string 
+  type        = string
   description = "The names of your private route table"
 }
 
 variable "nat_name" {
-  type = string 
+  type        = string
   description = "The Name of your nat internet gateway"
 }
 
 
 
 variable "sg_name" {
-    type = string 
-    description = "The name of your security group"
+  type        = string
+  description = "The name of your security group"
 }
 
-  // HTTP ipv6 Ingress Settings:
+// HTTP ipv6 Ingress Settings:
 
 variable "cidr_ipv6_ingress" {
-    type = string 
-    description = "The cidr block chosen for ipv6 for ingress routes"
+  type        = string
+  description = "The cidr block chosen for ipv6 for ingress routes"
 }
 
 variable "from_port_ipv6" {
-    type = number 
-    description = "The outgoing port number for ingress routes - ipv6"
+  type        = number
+  description = "The outgoing port number for ingress routes - ipv6"
 }
 
 variable "ip_protocol_ingress_ipv6" {
-    type = string
-    description = "The IP protocol chosen for ingress routes for ipv6"
+  type        = string
+  description = "The IP protocol chosen for ingress routes for ipv6"
 }
 
 variable "to_port_ipv6" {
-    type = number 
-    description = "The incoming port number for ingress routes - ipv6"
+  type        = number
+  description = "The incoming port number for ingress routes - ipv6"
 }
 
 
-  // HTTP ipv4 Ingress Settings:
+// HTTP ipv4 Ingress Settings:
 
 variable "cidr_ipv4_ingress" {
-    type = string 
-    description = "The cidr block chosen for ipv4 for ingress routes"
+  type        = string
+  description = "The cidr block chosen for ipv4 for ingress routes"
 }
 
 variable "from_port_ipv4" {
-    type = number 
-    description = "The outgoing port number for ingress routes - ipv4"
+  type        = number
+  description = "The outgoing port number for ingress routes - ipv4"
 }
 
 variable "ip_protocol_ingress_ipv4" {
-    type = string
-    description = "The IP protocol chosen for ingress routes for ipv4"
+  type        = string
+  description = "The IP protocol chosen for ingress routes for ipv4"
 }
 
 variable "to_port_ipv4" {
-    type = number 
-    description = "The incoming port number for ingress routes - ipv4"
+  type        = number
+  description = "The incoming port number for ingress routes - ipv4"
 }
 
 
-  // Local Route ipv6 Settings:
+// Local Route ipv6 Settings:
 
-variable "cidr_ipv6_local" { 
-    type = string
-    description = "The ipv6 cidr block for local routes"
+variable "cidr_ipv6_local" {
+  type        = string
+  description = "The ipv6 cidr block for local routes"
 }
 
-variable "from_localPort_ipv6" { 
-    type = number
-    description = "The outgoing local port number for ipv6"
+variable "from_localPort_ipv6" {
+  type        = number
+  description = "The outgoing local port number for ipv6"
 }
 
-variable "ip_protocol_local_ipv6" { 
-    type = string
-    description = "The IP protocol for local routes with ipv6"
+variable "ip_protocol_local_ipv6" {
+  type        = string
+  description = "The IP protocol for local routes with ipv6"
 }
 
-variable "to_localPort_ipv6" { 
-    type = number
-    description = "The incoming local port number for ipv6"
-}
-
-
-  // Local Route ipv4 Settings:
-
-variable "cidr_ipv4_local" { 
-    type = string
-    description = "The ipv4 cidr block for local routes"
-}
-
-variable "from_localPort_ipv4" { 
-    type = number
-    description = "The outgoing local port number for ipv4"
-}
-
-variable "ip_protocol_local_ipv4" { 
-    type = string
-    description = "The IP protocol for local routes with ipv4"
-}
-
-variable "to_localPort_ipv4" { 
-    type = number
-    description = "The incoming local port number for ipv4"
+variable "to_localPort_ipv6" {
+  type        = number
+  description = "The incoming local port number for ipv6"
 }
 
 
-  // ipv6 Egress Settings:
+// Local Route ipv4 Settings:
 
-  variable "cidr_ipv6_egress" {
-    type = string 
-    description = "The egress cidr block for ipv6"
-  }
+variable "cidr_ipv4_local" {
+  type        = string
+  description = "The ipv4 cidr block for local routes"
+}
 
-   variable "ip_protocol_egress_ipv6" {
-    type = string 
-    description = "The ip protocol for egress routes - ipv6"
-  }
+variable "from_localPort_ipv4" {
+  type        = number
+  description = "The outgoing local port number for ipv4"
+}
 
+variable "ip_protocol_local_ipv4" {
+  type        = string
+  description = "The IP protocol for local routes with ipv4"
+}
 
-
-  // ipv4 Egress Settings:
-
-    variable "cidr_ipv4_egress" {
-    type = string 
-    description = "The egress cidr block for ipv4"
-  }
-
-   variable "ip_protocol_egress_ipv4" {
-    type = string 
-    description = "The ip protocol for egress routes - ipv4"
-  }
+variable "to_localPort_ipv4" {
+  type        = number
+  description = "The incoming local port number for ipv4"
+}
 
 
-  // SSH Ingress Settings:
+// ipv6 Egress Settings:
 
-  variable "from_port_ssh" {
-    type = number 
-    description = "The outgoing port number for ingress SSH"
-  }
+variable "cidr_ipv6_egress" {
+  type        = string
+  description = "The egress cidr block for ipv6"
+}
 
-  variable "ip_protocol_ssh" {
-    type = string 
-    description = "The IP protocol used for SSH ingress "
-  }
-  
-  variable "to_port_ssh" {
-    type = number 
-    description = "The incoming port number for ingress SSH"
-  }
+variable "ip_protocol_egress_ipv6" {
+  type        = string
+  description = "The ip protocol for egress routes - ipv6"
+}
 
 
-  // Microservice Module variables:
 
-  variable "instance_type" {
-    type = string 
-    description = "The type of instance you want to use"
+// ipv4 Egress Settings:
+
+variable "cidr_ipv4_egress" {
+  type        = string
+  description = "The egress cidr block for ipv4"
+}
+
+variable "ip_protocol_egress_ipv4" {
+  type        = string
+  description = "The ip protocol for egress routes - ipv4"
+}
+
+
+// SSH Ingress Settings:
+
+variable "from_port_ssh" {
+  type        = number
+  description = "The outgoing port number for ingress SSH"
+}
+
+variable "ip_protocol_ssh" {
+  type        = string
+  description = "The IP protocol used for SSH ingress "
+}
+
+variable "to_port_ssh" {
+  type        = number
+  description = "The incoming port number for ingress SSH"
+}
+
+
+// Microservice Module variables:
+
+variable "instance_type" {
+  type        = string
+  description = "The type of instance you want to use"
 }
 
 
 variable "key_name" {
-    type = string 
-    description = "The key name you want to use for this instance"
-    default = "terraform-project-key"
+  type        = string
+  description = "The key name you want to use for this instance"
+  default     = "terraform-project-key"
 }
 
 variable "num_ami" {
-    type = number 
-    description = "The number of AMIs you want to create (matches the number of microservices)"
+  type        = number
+  description = "The number of AMIs you want to create (matches the number of microservices)"
 }
 
 variable "ami_names" {
-    type = list(string) 
-    description = "The names of the AMIs created"
+  type        = list(string)
+  description = "The names of the AMIs created"
 }
 
 variable "ec2_names" {
-    type = list(string) 
-    description = "The names of the Ec2 Instances created"
+  type        = list(string)
+  description = "The names of the Ec2 Instances created"
 }
 
 variable "dynamo_num" {
-    type = number 
-    description = "The number of DynamoDB tables you want to create"
+  type        = number
+  description = "The number of DynamoDB tables you want to create"
 }
 
 variable "dynamo_name" {
-    type = list(string) 
-    description = "The names of your DynamoDB tables"
+  type        = list(string)
+  description = "The names of your DynamoDB tables"
 }
 
 variable "num_ec2" {
-    type = number 
-    description = "The number of Ec2 instances you want to create for your microservices"
+  type        = number
+  description = "The number of Ec2 instances you want to create for your microservices"
 }
 
 variable "read_cap" {
-    type = number 
-    description = "The read capacity given to the users for the DynamoDB tables"
+  type        = number
+  description = "The read capacity given to the users for the DynamoDB tables"
 }
 
 variable "write_cap" {
-    type = number 
-    description = "The write capacity given to the users for the DynamoDB tables"
+  type        = number
+  description = "The write capacity given to the users for the DynamoDB tables"
 }
 
 variable "ami_filterName" {
-    type = string 
-    description = "The filter name for your AMI"
+  type        = string
+  description = "The filter name for your AMI"
 }
 
 
@@ -267,43 +267,43 @@ variable "ami_filterName" {
 
 
 variable "lb_name" {
-    type = string 
-    description = "The name of your loadbalancer"
+  type        = string
+  description = "The name of your loadbalancer"
 }
 
 variable "lb_listner_name" {
-    type = string 
-    description = "The name of the load-balance listner"
+  type        = string
+  description = "The name of the load-balance listner"
 }
 
 variable "num_targetGroups" {
-    type = number 
-    description = "The number of target groups you want to create"
+  type        = number
+  description = "The number of target groups you want to create"
 }
 
 variable "tg_name" {
-    type = list(string)
-    description = "The names of the target groups you created"
+  type        = list(string)
+  description = "The names of the target groups you created"
 }
 
 variable "tg_port" {
-    type = number 
-    description = "The port number for your target groups"
+  type        = number
+  description = "The port number for your target groups"
 }
 
 variable "tg_protocol" {
-    type = string 
-    description = "The protocol for your target groups"
+  type        = string
+  description = "The protocol for your target groups"
 }
 
 variable "lb_port" {
-    type = number 
-    description = "The port number for your load balancer listner"
+  type        = number
+  description = "The port number for your load balancer listner"
 }
 
 variable "lb_protocol" {
-    type = string 
-    description = "The protocol of your load balance listner"
+  type        = string
+  description = "The protocol of your load balance listner"
 }
 
 
@@ -312,62 +312,62 @@ variable "lb_protocol" {
 
 
 variable "min_size" {
-    type = number 
-    description = "The minimum number of instances you want"
+  type        = number
+  description = "The minimum number of instances you want"
 }
 
 variable "max_size" {
-    type = number 
-    description = "The maximum number of instances you want"
+  type        = number
+  description = "The maximum number of instances you want"
 }
 
 variable "desired_size" {
-    type = number 
-    description = "The desired number of isntances you want"
+  type        = number
+  description = "The desired number of isntances you want"
 }
 
 variable "health_check_grace_period" {
-    type = number 
-    description = "Time after instance comes into service before checking health"
+  type        = number
+  description = "Time after instance comes into service before checking health"
 
 }
 
 variable "health_check_type" {
-    type = string 
-    description = "The type of health check type you want on your auto scaling group"
-    default = "ELB"
+  type        = string
+  description = "The type of health check type you want on your auto scaling group"
+  default     = "ELB"
 }
 
 
 variable "num_launchTemplates" {
-    type = number 
-    description = "The number of launch templates  you want to create"
+  type        = number
+  description = "The number of launch templates  you want to create"
 }
 
 variable "num_asg" {
-    type = number
-    description = "The number of auto scaling groups you want to create"
+  type        = number
+  description = "The number of auto scaling groups you want to create"
 }
 
 variable "lt_name" {
-    type = list(string) 
-    description = "A list of names for the launch template resource"
+  type        = list(string)
+  description = "A list of names for the launch template resource"
 }
 
 variable "image_id" {
-    type = list(string)
-    description = "A list of AMi ids for the launch template resource"
+  type        = list(string)
+  description = "A list of AMi ids for the launch template resource"
 }
 
 
 variable "name_asg" {
-    type = list(string)
-    description = "The names of the auto scaling groups created"
+  type        = list(string)
+  description = "The names of the auto scaling groups created"
 }
 
 
 
 variable "ami_names_rep" {
-    type = list(string) 
-    description = "Rep names of AMIs"
+  type        = list(string)
+  description = "Rep names of AMIs"
 }
